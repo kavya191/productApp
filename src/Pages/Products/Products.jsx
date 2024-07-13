525b3cb32fac3c5d886a073c4bebe67f7c667148
kavya191 <kavyabose191@gmail.com>
2024-07-13 23:18:54 +0530

    final commit

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -123,7 +123,7 @@ const Products = () => {
           <Form.Select onChange={handleCategoryChange} value={queryParams.category || ''}>
             <option value=''>All Categories</option>
             {categories.map((category) => (
-              <option key={category} value={category}>{category}</option>
+              <option className='pro_option' key={category} value={category}>{category}</option>
             ))}
           </Form.Select>
         </Col>
